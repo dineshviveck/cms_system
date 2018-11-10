858e847b868ae1941a1caad8c358ce984a453561
Dinesh Kumar <dineshkumar.viveckanandan@photoninfotech.net>
2018-11-09 21:23:21 -0600

    Project base setup created

---
+++ src/main/java/com/cmssystem/cmssystem/CmssystemApplication.java
@@ -0,0 +1,12 @@
+package com.cmssystem.cmssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmssystemApplication.class, args);
+	}
+}
